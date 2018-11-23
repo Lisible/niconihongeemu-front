0a331ceb7c97c6326c142b0b8a5d283406e4d65f
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-24 00:36:12 +0100

    Basic project structure + started working on SearchBar component

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>NicoNihonGeemu</h1>
+    </header>
+    <div id="main-wrapper">
+        <search-bar></search-bar>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar/SearchBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style>
+#main-wrapper {
+    margin: 2em;
+}
+</style>
---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="search-bar">
+		<input type="text" name="search-input">
+		<select name="search-type">
+			<option value="any">*</option>
+			<option value="word">Word</option>
+			<option value="kanji">字</option>
+		</select>
+		<button id="search-button">Search</button>
+	</div>
+</template>
+
+<script src="./SearchBar.js"></script>
+<style src="./SearchBar.css"></style>
