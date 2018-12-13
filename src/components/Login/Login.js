c4284fcde9c6900340ba51269f07556b97885434
Clément SIBILLE <claymeuns@protonmail.com>
2018-12-13 13:00:32 +0100

    Fixed Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,9 +7,6 @@ export default {
 		}
 	},
 	methods: {
-		triggerAccountCreationRequest: function(event){
-			this.$parent.$emit('AccountCreationRequest');
-		},
 		triggerConnectionRequest: function(event){
 			this.$parent.$emit('ConnectionRequest', this.login, this.password);
 		}
--- src/main.js
+++ src/main.js
@@ -3,17 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-const Home = {
-    template: `
-        <search-bar></search-bar>
-        <deck-dashboard></deck-dashboard>
-    `
-}
-
-const routes = {
-    '/': Home
-}
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
