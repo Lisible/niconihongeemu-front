0641d37d009e0a5fc5b4bb05b68c0f00ccc97f0a
Clément SIBILLE <claymeuns@protonmail.com>
2019-01-13 17:07:16 +0100

    Document SearchBar, SearchResult, DeckStudy, DeckRecent

--- src/components/DeckRecent/DeckRecent.js
+++ src/components/DeckRecent/DeckRecent.js
@@ -13,13 +13,18 @@ export default {
         this.reloadDecks();
     },
     methods: {
+        /**
+         * Shows the details of a deck
+         * @param id The id of the deck
+         */
         showDeckDetails: function(id) {
             this.$eventBus.$emit('ShowDeckDetails', id);
         },
-        reloadDecks: function() {
-            setTimeout(async () => {
-                this.$data.decks = await DeckAPI.getUserDecks(AccessToken.token);
-            }, 100); 
+        /**
+         * Reloads the decks
+         */
+        reloadDecks: async function() {
+            this.$data.decks = await DeckAPI.getUserDecks(AccessToken.token);
         },
     }
 }
